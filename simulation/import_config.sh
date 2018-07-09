706c94ecf3bb333a4dd188667d0e2624526bb9ba
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2018-07-09 17:53:32 +0200

    Add manifest import
    T721CS

--- simulation/import_config.sh
+++ simulation/import_config.sh
@@ -3,4 +3,5 @@
 cp ./simulation/ticket721-contracts/chains.json ../ticket721/chains.json
 cp -r ./simulation/ticket721-contracts/dist ../ticket721/
 mv ./simulation/ticket721-contracts/.embark ../ticket721/
+mv ./simulation/manifest.json ../ticket721/
 
